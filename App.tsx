e8af2dac1c1ee8101c85ce7b97f9ebe62d858f1c
Yana Kapustian <yana2015.2004@gmail.com>
2025-02-23 18:01:09 +0200

    feat: add splash screen for ios

--- App.tsx
+++ App.tsx
@@ -1,15 +1,20 @@
 import {NavigationContainer} from '@react-navigation/native';
-import React from 'react';
+import React, {useCallback} from 'react';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import {TabNavigator} from './src/navigation/Router';
 import {Provider} from 'react-redux';
 import {store} from './src/store/store';
+import BootSplash from 'react-native-bootsplash';
 
 function App(): React.JSX.Element {
+  const onReady = useCallback(() => {
+    BootSplash.hide({fade: true});
+  }, []);
+
   return (
     <SafeAreaProvider>
       <Provider store={store}>
-        <NavigationContainer>
+        <NavigationContainer onReady={onReady}>
           <TabNavigator />
         </NavigationContainer>
       </Provider>
